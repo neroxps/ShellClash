425655caad78116e6dfcf0d2601f98c16ea3155f
Bot <juewuy@gmail.com>
2023-02-11 09:18:21 +0000

    自动更新最新Dashboard、地址库、根证书

---
+++ bin/Yacd-meta-gh-pages/assets/useRemainingViewPortHeight-560f5200.js
@@ -0,0 +1,1 @@
+import{h as r}from"./index-f72f8f03.js";const{useState:s,useRef:u,useCallback:a,useLayoutEffect:c}=r;function d(){const t=u(null),[n,i]=s(200),e=a(()=>{const{top:o}=t.current.getBoundingClientRect();i(window.innerHeight-o)},[]);return c(()=>(e(),window.addEventListener("resize",e),()=>{window.removeEventListener("resize",e)}),[e]),[t,n]}export{d as u};
